d873fa083bb3c597c678474174344547bbed09af
brnpsantos1993 <brnpsantos1993@gmail.com>
2022-11-27 10:50:25 -0300

    DEVOPS-0002: fix pipeline

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -12,14 +12,14 @@ terraform {
   }
 }
 
-resource "aws_s3_bucket" "bucket" {
-  bucket = "my-tf-test-bucket-bruno"
+# resource "aws_s3_bucket" "bucket" {
+#   bucket = "my-tf-test-bucket-bruno"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
+#   tags = {
+#     Name        = "My bucket"
+#     Environment = "Dev"
+#   }
+# }
 
 resource "aws_s3_bucket_acl" "example" {
   bucket = aws_s3_bucket.bucket.id
