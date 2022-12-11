e1c79db417ffb3ab2f8190e187c3df2f834b2089
brnpsantos1993 <brnpsantos1993@gmail.com>
2022-12-11 10:37:37 -0300

    DEVOPS-005: add module ec2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,10 @@
+# Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
 
-# data "aws_ami" "ubuntu" {
-#   most_recent = true
-
-#   filter {
-#     name   = "name"
-#     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-#   }
-
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
-
-#   owners = ["099720109477"] # Canonical
-# }
-
-resource "aws_instance" "web" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "HelloWorld"
-  }
+module "ec2" {
+  source        = "../module/ec2"
+  ami           = "ami-0b0dcb5067f052a63"
+  instance_type = "t2.micro"
 }
